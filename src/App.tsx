668fec9efe22ae07fe4d40106c97fa9ddbebb234
Christian Stam <cstam2@wgu.edu>
2023-11-03 22:31:10 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-tr from-slate-900 to-slate-700 text-slate-300">
+      <div>
+      <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-slate-300 pr-5 text-5xl text-white font-bold">Coming Soon...</h1>
+      </div>
+      
+    </div>
+  );
+}
+export default App;
